87248de56f805bce84d81ee2dc02d45117dfc466
Mayank Shigaonker <shigaonkerMayank@gmail.com>
2026-01-06 00:16:35 +0530

    Refactor main.py to enhance logging; replace print statements with log calls for error handling and status updates.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sys
+
+from Logger import LogLevel, log
 from TicketFolderHandler import TicketFolderHandler
 from common import TICKET_FOLDER
 
@@ -19,16 +21,14 @@ def main() -> None:
     try:
         observer.start()
     except FileNotFoundError as error:
-        print(
-            f"Error: '{TICKET_FOLDER}' doesn't exist hence cannot monitor it")
-        print(error)
-        print("Exiting...")
+        log(LogLevel.Error,
+            f"'{TICKET_FOLDER}' doesn't exist hence cannot monitor it {error}. Exiting...")
         sys.exit(-1)
 
     try:
         observer.join()
     except KeyboardInterrupt:
-        print("\nStopping")
+        log(LogLevel.Status, "Stopping")
 
 
 if __name__ == "__main__":
