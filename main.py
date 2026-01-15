b56b76134bd5c90bcf82e10e02dba6ce105bbcbc
Mayank Shigaonker <shigaonkerMayank@gmail.com>
2026-01-15 22:05:23 +0530

    Fixes #2

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 import sys
 
+from Configuration import Configuration
 from ConfigurationHandler import handler as config_handler
 from Logger import LogLevel, log
 from TicketFolderHandler import TicketFolderHandler
@@ -7,7 +9,18 @@ from TicketFolderHandler import TicketFolderHandler
 from watchdog.observers import Observer
 
 
+def cache_cleanup(config: Configuration) -> None:
+    try:
+        for file in config.cache_folder.iterdir():
+            if file.is_file() and datetime.now() - datetime.fromtimestamp(file.stat().st_mtime) > config.cache_data_refresh_time:
+                file.unlink(missing_ok=True)
+    except Exception as error:
+        log(LogLevel.Warning, config,
+            f"Failure to cleanup outdated cache file: {error}")
+
 def main() -> None:
+    cache_cleanup(config_handler.config)
+
     observer = Observer()
 
     observer.schedule(
