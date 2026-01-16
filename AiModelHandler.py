4bf33ef8d4d4eb55d046fd66c5d02d51e3ba41c2
Mayank Shigaonker <shigaonkerMayank@gmail.com>
2026-01-16 11:32:35 +0530

    Add caching to AiModelHandler along with quota exhausted error handling

--- AiModelHandler.py
+++ AiModelHandler.py
@@ -5,8 +5,10 @@ from typing import Self
 
 from google import genai
 from google.api_core import exceptions
+from google.genai.errors import ClientError
 
 from Configuration import Configuration
+from FileCache import FileCache
 from Logger import LogLevel, log
 from common import calculate_backoff
 
@@ -35,38 +37,58 @@ class Model:
         if self._client is None:
             self._client = self._get_client(config)
 
-        log(LogLevel.Status, config, f"Asking {config.ai_model} for help")
+        def impl(config: Configuration) -> str:
+            log(LogLevel.Status, config, f"Asking {config.ai_model} for help")
 
-        for attempt in range(config.max_retries_for_network_requests):
-            try:
-                response = self._client.models.generate_content(
-                    model=config.ai_model,
-                    contents=[
-                        genai.types.Part.from_bytes(
-                            data=ticket_fp.read_bytes(),
-                            mime_type="application/pdf"
-                        ),
-                        prompt
-                    ],
-                    config=genai.types.GenerateContentConfig(temperature=0.1)
-                )
+            for attempt in range(config.max_retries_for_network_requests):
+                try:
+                    if self._client is None:
+                        raise Exception(f"self._client in None in {__file__}")
 
-                if response.text is None:
-                    raise Exception("Response was obtained as None")
+                    response = self._client.models.generate_content(
+                        model=config.ai_model,
+                        contents=[
+                            genai.types.Part.from_bytes(
+                                data=ticket_fp.read_bytes(),
+                                mime_type="application/pdf"
+                            ),
+                            prompt
+                        ],
+                        config=genai.types.GenerateContentConfig(
+                            temperature=0.1)
+                    )
 
-                return response.text
-            except exceptions.ResourceExhausted as error:
-                log(LogLevel.Warning, config, f"API quota exhausted: {error}")
+                    if response.text is None:
+                        raise Exception("Response was obtained as None")
 
-            except exceptions.GoogleAPIError as error:
-                log(LogLevel.Warning, config, f"Google API Error: {error}")
+                    return response.text
+                except exceptions.ResourceExhausted as error:
+                    log(LogLevel.Warning, config,
+                        f"API quota exhausted: {error}")
 
-            except Exception as error:
-                log(LogLevel.Warning, config, f"Some error occured: {error}")
+                except exceptions.GoogleAPIError as error:
+                    log(LogLevel.Warning, config, f"Google API Error: {error}")
 
-            log(LogLevel.Status, config,
-                f"Retrying in {calculate_backoff(attempt)} seconds")
-            time.sleep(calculate_backoff(attempt))
+                except ClientError as error:
+                    if error.code == 429:
+                        models = ["gemini-2.5-flash",
+                                  "gemini-2.5-flash-lite", "gemini-3-flash"]
+                        log(LogLevel.Warning, config,
+                            f"Exceeded quota for current model. Trying a new one: {models[attempt % len(models)]}...")
+                        config.ai_model = models[attempt % len(models)]
+                    else:
+                        log(LogLevel.Warning, config,
+                            f"Some client error occured: {error}")
 
-        raise Exception(
-            f"Failure to parse ticket from AI Model after {config.max_retries_for_network_requests} retries")
+                except Exception as error:
+                    log(LogLevel.Warning, config,
+                        f"Some error occured: {error}")
+
+                log(LogLevel.Status, config,
+                    f"Retrying in {calculate_backoff(attempt)} seconds")
+                time.sleep(calculate_backoff(attempt))
+
+            raise Exception(
+                f"Failure to parse ticket from AI Model after {config.max_retries_for_network_requests} retries")
+
+        return FileCache(ticket_fp.stem, impl, lambda x: x, lambda x: x, config).data
